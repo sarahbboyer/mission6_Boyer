6ff70d4a683a9732cdd829a09c1d45465646aa65
sarahbboyer <166653344+sarahbboyer@users.noreply.github.com>
2025-02-20 23:50:34 -0700

    idk but fixed

--- mission6_assignment/Models/Movie.cs
+++ mission6_assignment/Models/Movie.cs
@@ -32,11 +32,11 @@ public class Movie
     public string? Rating { get; set; }
   
     [Required(ErrorMessage = "Edited is required")]
-    public int Edited { get; set; }
+    public bool Edited { get; set; }
     public string? LentTo { get; set; }
   
     [Required(ErrorMessage = "CopiedToPlex is required")]
-    public int CopiedToPlex { get; set; }
+    public bool CopiedToPlex { get; set; }
   
     [StringLength(25, ErrorMessage = "Notes can't be longer than 25 characters.")]
     public string? Notes { get; set; }
